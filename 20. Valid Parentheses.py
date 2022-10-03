092fd870ab52d51c97dc7559650d4135b2519dbc
Shubankar B <rohit.shubankar@gmail.com>
2022-10-03 19:14:32 +0530

    Valid Parantheses

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_brackets = set(['(','[','{'])
+        stack = []
+        for char in s:
+            if char in open_brackets: 
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                current_char = stack.pop()
+                if current_char =='(':
+                    if char!=')':
+                        return False
+                if current_char =='[':
+                    if char!=']':
+                        return False
+                if current_char =='{':
+                    if char!='}':
+                        return False
+        if stack:
+            return False
+        return True
